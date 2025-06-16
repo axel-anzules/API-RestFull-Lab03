a2149e5d714899297401d19679c1185614974cd5
axel-anzules <axel.anzules2018@uteq.edu.ec>
2025-06-16 17:24:13 -0500

    Set Identity

---
+++ app/src/main/java/com/example/api_restfull_lab03/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.api_restfull_lab03;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.api_restfull_lab03.WebServices.Asynchtask;
+import com.example.api_restfull_lab03.WebServices.WebService;
+
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onClick(View view) {
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService(
+                "https://revistas.uteq.edu.ec/ws/login.php?usr=cristian&pass=123",
+                datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET");
+    }
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView txtResp = findViewById(R.id.txtResp);
+        txtResp.setText(result);
+    }
+}
